b31f0473bbac36edf735591b307c571896abc0d7
Camile Coelho <camilecoelho23@gmail.com>
2023-04-06 00:25:41 -0300

    Add project files.

---
+++ TriangulosPoo.ConsoleApp/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace TriangulosPoo.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+
+            Triangulo triangulo = new Triangulo();
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("_________________________________________________________________________________");
+                Console.WriteLine("\n                         Verificação Triângulos                                ");
+                Console.WriteLine("_________________________________________________________________________________");
+
+                triangulo.LerLados();
+
+                if (triangulo.VerificarTriangulo())
+                {
+                    Console.WriteLine("\n   Os valores informados formam um triângulo");
+
+                    if (triangulo.VerificarEquilatero())
+                        Console.WriteLine("\n   O triângulo é equilátero");
+                    else if (triangulo.VerificarIsosceles())
+                        Console.WriteLine("\n   O triângulo é isósceles");
+                    else
+                        Console.WriteLine("\n   O triângulo é escaleno");
+                    break;
+                }
+                else
+                {
+                    ExibirMensagemEmVermelho("\n   Os valores informados não formam um triângulo");
+                }
+            }
+
+            Console.ResetColor();
+        }
+
+        static void ExibirMensagemEmVermelho( string mensagem)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Write(mensagem);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.ReadLine();
+        }
+    }    
+}
---
+++ TriangulosPoo.ConsoleApp/Triangulo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangulosPoo.ConsoleApp
+{
+    internal class Triangulo
+    {
+        double lado1, lado2, lado3;
+
+        public void LerLados()
+        {
+            Console.Write("\n   Digite o comprimento do primeiro lado do triângulo: ");
+            lado1 = double.Parse(Console.ReadLine());
+            Console.Write("\n   Digite o comprimento do segundo lado do triângulo: ");
+            lado2 = double.Parse(Console.ReadLine());
+            Console.Write("\n   Digite o comprimento do terceiro lado do triângulo: ");
+            lado3 = double.Parse(Console.ReadLine());
+        }
+        public bool VerificarTriangulo()
+        {
+            if (lado1 < lado2 + lado3 && lado2 < lado1 + lado3 && lado3 < lado1 + lado2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool VerificarEquilatero()
+        {
+            if (lado1 == lado2 && lado2 == lado3)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool VerificarIsosceles()
+        {
+            if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
